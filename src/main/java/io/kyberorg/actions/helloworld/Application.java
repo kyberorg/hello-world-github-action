3f7dabc636f95533015ba27c7148ce74226f4a08
Alex Muravya <root@kyberorg.io>
2021-02-05 19:51:43 +0200

    Test

--- src/main/java/io/kyberorg/actions/helloworld/Application.java
+++ src/main/java/io/kyberorg/actions/helloworld/Application.java
@@ -1,5 +1,7 @@
 package io.kyberorg.actions.helloworld;
 
+import kong.unirest.Unirest;
+
 public class Application  {
     public static void main(String[] args) {
         String name;
@@ -8,6 +10,8 @@ public class Application  {
         } else {
             name = "Unknown";
         }
+
+        Unirest.get("https://google.com");
         System.out.println("Hello, "+name);
 	    System.exit(0);
     }
